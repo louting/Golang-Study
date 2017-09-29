9d8599b2ef78fcb3183c3fa5b5e4cde3d444fcca
tony <tony@tonydeMacBook-Pro.local>
2017-09-29 22:45:17 +0800

    modify

--- Struct_Example/define-struct.go
+++ Struct_Example/define-struct.go
@@ -30,7 +30,16 @@ func (p *Polar) Scale() float64 {
 func main() {
 	point := &Point{3, 5}
 	var polar Polar
-	polar.z = 0.222222
+	/*
+		此处不能使用 var polar *Polar 这样的定义方式，不然下面的语句
+		polar.z 属于一个指针，应该不能为指针赋值
+		报错为：runtime error: invalid memory address or nil pointer dereference
+	*/
+	polar.z = 10
 	polar.point = point
 	fmt.Println(polar.Scale())
 }
+
+/* result
+150
+*/
