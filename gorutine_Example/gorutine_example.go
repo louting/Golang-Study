26b078717ce4c507c14dda3c3c0cacd81a3e5b2f
louting <tony.lt.cn@gmail.com>
2017-10-17 17:19:19 +0800

    add

---
+++ gorutine_Example/gorutine_example.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	go spinner(100 * time.Millisecond)
+	const n = 45
+	fibN := fib(n)
+	fmt.Printf("\rFibonacci(%d) = %d\n", n, fibN)
+}
+
+func spinner(delay time.Duration) {
+	for {
+		for _, r := range `-\|/` { // `` 在golang里面的意思是 字符字面量。
+			fmt.Printf("\r%c", r) // 这里的\r 在linux内代表软空格，返回本行首。%c 相应Unicode码点所表示的字符。
+			time.Sleep(delay)
+		}
+	}
+}
+
+func fib(x int) int {
+	if x < 2 {
+		return x
+	}
+	return fib(x-1) + fib(x-2)
+}
